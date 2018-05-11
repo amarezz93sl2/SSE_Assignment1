7b37696b65da575e0216da631626bdc3052ce748
amarezz93sl2 <39181683+amarezz93sl2@users.noreply.github.com>
2018-05-11 11:36:34 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+
+$input_email = $_POST['input_email'];
+$input_password = $_POST['input_password'];
+$input_token = $_POST['input_token'];
+
+
+
+ 
+if ($input_token == $_SESSION['token'])
+{
+    
+    /* Valid Token */
+     echo "Valid Token <br>";
+     
+        unset($_SESSION["token"]);
+        session_destroy();
+     
+    
+    if($input_email == "IT13111502@my.sliit.lk" && $input_password == "IT13111502"){
+        
+        
+         echo "Valid Email & Password";
+        
+        
+        
+    }else{
+        
+        
+         echo "Invalid Email & Password";
+        
+        
+        
+    }
+    
+    
+    
+    
+}else {
+    
+    
+     /* Invalid Token */
+    
+    echo "Invalid Token";
+    
+    
+    
+}
+ 
+?>
